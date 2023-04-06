5bfb0cc66570704d9647ee60e20ee0d58a7ab843
Trav798 <68037803+Trav798@users.noreply.github.com>
2023-04-06 11:35:35 -0700

    Changed states around

--- src/helper_functions.h
+++ src/helper_functions.h
@@ -16,8 +16,6 @@ float angleBetween(Vector2f vec1, Vector2f vec2);
 float distanceBetween(Vector2f origin, Vector2f point);
 float rad2deg(float rad);
 int minSpeed(int input, int min);
-int setServo3Speed(Servo servoMotor, int speed);
-int setServo4Speed(Servo servoMotor, int speed);
 float twoPointSlope(Vector2f point1, Vector2f point2);
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -25,22 +25,26 @@ using namespace std;
 #define LEFT_SWITCH 18
 #define RIGHT_SWITCH 19
 #define GATE_PIN 22 
+#define HOME_POS (1800,200)
 
 
 #define MOVEMENT_TIMEOUT 500
 #define ROTATION_EPSILON .2
 #define DRIVE_EPSILON 250
 
-void rotateState(unsigned long lastRotUpdate);
-void driveState(unsigned long lastPosUpdate);
+void ballState(unsigned long lastRotUpdate);
 void backupState();
-void standbyState();
+void standbyState(int numBalls);
 void approachState();
 int setServo3Speed(int speed);
 int setServo4Speed(int speed);
 void leftLimitCallback();
 void rightLimitCallback();
 Vector2f chooseBallTarget(BallPosition balls[NUM_BALLS], int numBalls);
+float moveToTarget();
+void stopRobot();
+void openGate();
+void closeGate();
 
 
 
@@ -70,7 +74,7 @@ void setup() {
 
   gateMotor.attach(GATE_PIN);
   delay(100);
-  gateMotor.write(90); // 80 is closed, 0 is open
+  closeGate();
   delay(500);
   setPIDgains1(kp,ki,kd);
 
@@ -85,22 +89,34 @@ void setup() {
 }
 
 
+
+
+
+
+
+
 //////////////////////////////// GLOBAL VARIABLES ///////////////////////////////////
 
-CurrentState state = ROTATE;
+CurrentState state = BALL;
 RobotState robotState;
 BallPosition ballArray[NUM_BALLS];
 
-Vector2f targetPos(1000,1000);
+Vector2f targetPos(HOME_POS);
 
 const float kpDrive = .2;
 const float kpRot = 12;
+bool rotating = true;
 
-unsigned long lastPosUpdate = 0;
-unsigned long lastRotUpdate = 0;
-
-unsigned long updateTarget = millis() + 2500;
+unsigned long updateTarget = millis();
+bool freezeTarget = false;
 int ballCount = 0;
+
+
+
+
+
+
+
 
 
 
@@ -109,130 +125,69 @@ void loop() {
   Serial.println("------------------------------");
   // Update robot info
   RobotPose poseData =  getRobotPose(MY_ROBOT_ID);
-  lastPosUpdate = robotState.updateRobotPosition(poseData.x, poseData.y) ? millis() : lastPosUpdate;
-  lastRotUpdate = robotState.updateRobotRotation(((float)poseData.theta)/1000) ? millis() : lastRotUpdate; 
+  unsigned long lastPosUpdate = robotState.updateRobotPosition(poseData.x, poseData.y) ? millis() : lastPosUpdate;
+  robotState.updateRobotRotation(((float)poseData.theta)/1000); 
   // Serial.printf("raw theta: %d\n", poseData.theta);
 
 
-  // Update ball info
+  // Update ball info only if all requirements are met
   int numBalls = getBallPositions(ballArray);
-  if (millis() > updateTarget && numBalls > 0) { // TODO handle multiple balls
+  if (millis() > updateTarget && !freezeTarget && numBalls > 0) { 
     targetPos = chooseBallTarget(ballArray, numBalls);
     // targetPos(0) = (float)ballArray[0].x;
     // targetPos(1) = (float)ballArray[0].y;
-    updateTarget = false;
+    updateTarget = millis() + 2500;
+  } else if (numBalls == 0) {
+    state = STANDBY;
   }
   Serial.printf("targetPos: x: %f, y: %f\n", targetPos(0), targetPos(1));
 
 
   // Handle different states
-  // TODO update state diagram to include nested states
-  if (state == ROTATE) {
-    Serial.println("ROTATE state");
-    rotateState(lastRotUpdate);
-  } else if (state == DRIVE) {
-    Serial.println("DRIVE state");
-    driveState(lastPosUpdate);
+  if (state == BALL) {
+    Serial.println("BALL state");
+    ballState(lastPosUpdate);
   } else if (state == STANDBY) {
     Serial.println("STANDBY state");
-    standbyState();
+    standbyState(numBalls);
   } else if (state == BACKUP) {
     Serial.println("BACKUP state");
     backupState();
   } else if (state == APPROACH) {
     Serial.println("APPROACH state");
     approachState();
-  }  else {
+  } else if (state == HOME) {
+    Serial.println("HOME state");
+    homeState();
+  } else {
     Serial.println("Unknown state");
   }
 
-  // // // IR SENSOR STUFF
-  // // // Scan 180 degrees in front of robot with ir sensor and
-  // // // store results in the array irReadings
-  // // float irReadings[15];
-  // // int index = 0;
-  // // setSetpoint1(setpoint);
-  // // for (int i = -700; i <= 700; i += 100) {
-  // //   irReadings[index] = convertVoltage2Distance((float)analogRead(IR_PIN_IN));
-  // //   index++;
-  // //   setpoint = i;
-  // //   setSetpoint1(setpoint);
-  // //   delay(200);
-  // // }
-  // // irReadings[index] = convertVoltage2Distance(analogRead(IR_PIN_IN));
   delay(100);
 }
 
 
 
+
+
+
+
+
 ////////////////////////// STATES /////////////////////
-void rotateState(unsigned long lastRotUpdate) {
-  bool rotationComplete = false;
-  // check if the robots position is being updated
-  if (millis() - lastRotUpdate < MOVEMENT_TIMEOUT) {
-    // calculates the current pointing error
-    float thetaError = angleBetween(robotState.getFrontVector(), targetPos - robotState.getPosition());
-
-    int servoSpeed = (int)(kpRot*thetaError);
-    // int servo4Speed = (int)(-1*kpRot*thetaError);
-    Serial.printf("servo3Speed(right): %d\n", setServo3Speed(servoSpeed)); // should work, maybe not
-    Serial.printf("servo4Speed(left): %d\n", setServo4Speed(-servoSpeed));
-    // Serial.printf("theta error: %f\n", thetaError);
-
-    // if the pointing error is low enough, start driving towards the ball
-    rotationComplete = abs(thetaError) < ROTATION_EPSILON;
+void ballState(unsigned long lastPosUpdate) {
+  float distanceError = 999999;
+  if (millis() - lastPosUpdate < MOVEMENT_TIMEOUT) {
+    distanceError = moveToTarget();
   } else {
-    // stop the robot if the camera has lost contact
-    servo3.writeMicroseconds(1500);
-    servo4.writeMicroseconds(1500);
-    Serial.println("Rotation not updating, stopping the robot");
+    stopRobot();
   }
 
-  // change the state if the rotation is done
-  if (rotationComplete) {
-    state = DRIVE;
-  }
-}
-
-void driveState(unsigned long lastPosUpdate) {
-  bool driveComplete = false;
-  // calculate the pointing error to decide if we are too far off track and need to rotate again
-  float thetaError = angleBetween(robotState.getFrontVector(), targetPos - robotState.getPosition());
-  Serial.printf("thetaError: %f\n", thetaError);
-  if (abs(thetaError) > 2*ROTATION_EPSILON) {
-    // change state to ROTATE if we are too far off track
-    Serial.println("Off course, rotating");
-    state = ROTATE;
-    return;
-  } else if (millis() - lastPosUpdate < MOVEMENT_TIMEOUT) {
-    // calculate distance from ball
-    float distanceError = distanceBetween(robotState.getPosition(), targetPos);
-
-    // do control loop, set robot linear speed
-    int servoSpeed = (int)(kpDrive*distanceError);
-    Serial.printf("servoSpeed: %d\n", servoSpeed);
-    setServo3Speed(servoSpeed);
-    setServo4Speed(servoSpeed);
-
-    // decide if we have reached the ball
-    driveComplete = distanceError < DRIVE_EPSILON;
-  } else {
-    // if the camera has lost contact, stop the robot
-    servo3.writeMicroseconds(1500);
-    servo4.writeMicroseconds(1500);
-    Serial.println("Position not updating");
-  }
-
-  // if we have reached the ball, stop
-  if (driveComplete) {
-    // Vector2f vector1 =  targetPos - robotState.getPosition();
-    // vector1 *= 1.5;
-    // Vector2f targetPos = robotState.getPosition() + vector1;
+  if (distanceError < DRIVE_EPSILON) {
+    rotating = true;
     state = APPROACH;
   }
 }
 
-// TODO make this better and not take 5 seconds
 void backupState() {
 
   // if (digitalRead(RIGHT_SWITCH) == LOW){
@@ -246,8 +201,8 @@ void backupState() {
   //   servo4.writeMicroseconds(-100);
   // }
   
-  servo3.writeMicroseconds(-100);
-  servo4.writeMicroseconds(-100);
+  setServo3Speed(-100);
+  setServo4Speed(-100);
   delay(2000);
 
   int distances[18]; // Array to store distances
@@ -271,27 +226,56 @@ void backupState() {
 
 }
 
-void standbyState() {
-  servo3.writeMicroseconds(1500);
-  servo4.writeMicroseconds(1500);
+void standbyState(int numBalls) {
+  stopRobot();
   digitalWrite(INDICATOR_PIN, HIGH);
+  if (numBalls > 0) {
+    state = BALL;
+  }
 }
 
 
 void approachState() {
 //  Vector2f currentPos = robotState.getPosition();
-  gateMotor.write(15);
+  openGate();
   setServo3Speed(100);
   setServo4Speed(100);
   delay(1000);
-  gateMotor.write(90); 
+  closeGate();
   delay(500);
-  servo3.writeMicroseconds(1500);
-  servo4.writeMicroseconds(1500);
-  state = ROTATE;
-  updateTarget = millis();
+  stopRobot();
+
   ballCount++;
+  if (ballCount > 3) {
+    freezeTarget = true;
+    targetPos = Eigen::Vector2f(HOME_POS);
+    state = HOME;
+  } else {
+    state = BALL;
+  }
  }
+
+ void homeState() {
+  float distanceError = moveToTarget();
+
+  if (distanceError < 1.5*DRIVE_EPSILON) {
+    openGate();
+    setServo3Speed(-100);
+    setServo4Speed(-100);
+    delay(1500);
+    closeGate();
+
+    freezeTarget = false;
+    rotating = true;
+    state = BALL;
+  }
+ }
+
+
+
+
+
+
 
 
 
@@ -331,4 +315,56 @@ Vector2f chooseBallTarget(BallPosition balls[NUM_BALLS], int numBalls) {
 
   std::cout << Eigen::Vector2f((float)balls[smallest].x, (float)balls[smallest].y) << std::endl;
   return Eigen::Vector2f((float)balls[smallest].x, (float)balls[smallest].y);
+}
+
+float moveToTarget() {
+  // calculate the pointing error to decide if we are too far off track and need to rotate again
+  float thetaError = angleBetween(robotState.getFrontVector(), targetPos - robotState.getPosition());
+  // calculate distance from ball
+  float distanceError = distanceBetween(robotState.getPosition(), targetPos);
+  Serial.printf("thetaError: %f\n", thetaError);
+  if (rotating) {
+    // calculate servo speed
+    int servoSpeed = (int)(kpRot*thetaError);
+    Serial.printf("servo3Speed(right): %d\n", setServo3Speed(servoSpeed));
+    Serial.printf("servo4Speed(left): %d\n", setServo4Speed(-servoSpeed));
+    // Serial.printf("theta error: %f\n", thetaError);
+
+    // if the pointing error is low enough, start driving towards the ball
+    if (abs(thetaError) < ROTATION_EPSILON) {
+      rotating = false;
+    }
+  } else {
+
+    // do control loop, set robot linear speed
+    int servoSpeed = (int)(kpDrive*distanceError);
+    Serial.printf("servoSpeed: %d\n", servoSpeed);
+    setServo3Speed(servoSpeed);
+    setServo4Speed(servoSpeed);
+
+    // start rotating again if too far off track
+    if (abs(thetaError) > 2*ROTATION_EPSILON) {
+      Serial.println("Off course, rotating");
+      rotating = true;
+      return;
+    }
+  }
+  return distanceError;
+}
+
+void stopRobot() {
+  servo3.writeMicroseconds(1500);
+  servo4.writeMicroseconds(1500);
+}
+
+void openGate() {
+  gateMotor.write(15);
+}
+
+void closeGate() {
+  gateMotor.write(90);
+}
+
+void edgeScan(Vector2f edges[2]) {
+  
 }
